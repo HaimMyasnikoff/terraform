84450e0d7fd2057f1b6bd1e5a7c2364ca0c9a37a
Vibu <vibu@ukr.net>
2018-03-14 17:30:59 +0000

    Add SG

--- main.tf
+++ main.tf
@@ -7,3 +7,13 @@ resource "aws_instance" "sigma" {
   instance_type = "t2.micro"
 tags {    Name = "terraform.sigma"  }
 }
+
+resource "aws_security_group" "instance" {  
+name = "terraform-example-instance"  
+ingress {    
+		from_port   = 8080    
+		to_port     = 8080    
+		protocol    = "tcp"    
+		cidr_blocks = ["0.0.0.0/0"]  
+		}
+}
